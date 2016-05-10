86d507e7dcd6ef4b44b1c4dd4427c1d9c49935a7
kraken108 <jakob-danielsson@hotmail.com>
2016-05-10 21:49:29 +0200

    lagt till val om man vill connecta till servern eller inte när man startar klienten

--- integration/spelet/network.c
+++ integration/spelet/network.c
@@ -12,7 +12,7 @@ int uncomplete_string(char tmp[])
     return 1;
 }
 
-void networkInit(Network *client)
+int networkInit(Network *client,Player *man)
 {
     SDLNet_Init();
     client->tcpset=SDLNet_AllocSocketSet(10);
@@ -41,13 +41,13 @@ void networkInit(Network *client)
         return 0;
     }
 
-    if(!(client->rcvpack = SDLNet_AllocPacket(1024))
+    if(!(client->rcvpack = SDLNet_AllocPacket(1024)))
     {
         printf("Could not allocate receiving packet\n");
         return 0;
     }
-    client->sendpack.host = ip.host;
-    client->sendpack.port = ip.port;
+    client->sendpack->address.host = ip.host;
+    client->sendpack->address.port = ip.port;
 
     //Försöker connecta till servern
     if(!(client->tcpsock = SDLNet_TCP_Open(&tcpip)))
@@ -55,9 +55,9 @@ void networkInit(Network *client)
         printf("Couldnt connect to server\n");
         return 0;
     }
-    SDLNet_TCP_Recv(tcpsock,tmp,1024);
-    sscanf(tmp,"%d",&(client->id));
-    printf("my ID: %d\n",client->id);
+    SDLNet_TCP_Recv(client->tcpsock,tmp,1024);
+    sscanf(tmp,"%d",&(man->id));
+    printf("my ID: %d\n",man->id);
 
     SDLNet_TCP_AddSocket(client->tcpset,client->tcpsock);
     SDLNet_UDP_AddSocket(client->udpset,client->udpsock);
--- integration/spelet/newmain.c
+++ integration/spelet/newmain.c
@@ -2,7 +2,7 @@
 
 extern void init(Player *);
 extern void doRender(Player *man,Bullet b[]);
-extern int processEvents(Player *man,Bullet b[]);
+extern int processEvents(Player *man,Bullet b[],int moved);
 
 extern void clearCartridge(Bullet ammo[]);
 extern void Quit();
@@ -13,60 +13,73 @@ extern void loadAmmo(Bullet b[]);
 extern SDL_Texture *initBullet();
 
 extern void updateLogic(Player *p, Bullet b[]);
-extern void networkInit(Network *client);
+extern int networkInit(Network *client,Player *man);
 
 int global = 0;
 int main(int argc, char *argv[])
 {
+    int done = 0;
+    Player player;
+    Network client;
+    int choice;
+    int newline;
+    printf("Vill du connecta till servern? 1=JA 0=NEJ");
+    scanf("%d",&choice);
+    scanf("%c",&newline);
 
-  Player player;
-
-  init(&player);
-
-  bullet.texture=initBullet();
-  Bullet ammo[20];
-
-  int done = 0;
-
-  //*******INIT NETWORK***************
-  Network client;
-  int moved = 0;
-  if(!(networkInit(&client)))
-  {
-      done = 1;
-  }
+    if(choice==1)
+    {
+        //*******INIT NETWORK***************
 
 
 
+        if(!(networkInit(&client,&player)))
+        {
+            done = 1;
+        }
 
-  //**********************************
+    }//**********************************
 
-  //link(ammo);
-  //Event loop
-  clearCartridge(ammo);
+    init(&player);
 
-  while(!done)
-  {
-    done = processEvents(&player,ammo,&change);
-    //if(moved)
-   // {
-     //   send_position(&player);
-     //   moved = 0;
-   // }
-    updateLogic(&player,ammo);
-    doRender(&player,ammo);
-    //don't burn up the CPU
-    SDL_Delay(10);
-  }
-    SDLNet_FreeSocketSet(client->udpset);
-    SDLNet_FreeSocketSet(client->tcpset);
-    SDLNet_UDP_Close(client->udpsock);
-    SDLNet_TCP_Close(client->tcpsock);
+    bullet.texture=initBullet();
+    Bullet ammo[20];
+
+
+    int moved = 0;
+
+
+
+
+
+
+
+    //link(ammo);
+    //Event loop
+    clearCartridge(ammo);
+
+    while(!done)
+    {
+        done = processEvents(&player,ammo,&moved);
+        //if(moved)
+        // {
+        //   send_position(&player);
+        //   moved = 0;
+        // }
+        updateLogic(&player,ammo);
+        doRender(&player,ammo);
+        //don't burn up the CPU
+        SDL_Delay(10);
+    }
+    SDLNet_FreeSocketSet(client.udpset);
+    SDLNet_FreeSocketSet(client.tcpset);
+    SDLNet_UDP_Close(client.udpsock);
+    SDLNet_TCP_Close(client.tcpsock);
     SDLNet_Quit();
     Quit();
 
-  // Close and destroy the window
+    // Close and destroy the window
 
-  return 0;
+    return 0;
 }
 
