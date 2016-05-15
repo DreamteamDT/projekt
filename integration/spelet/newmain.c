e3e7af8420519c204a9a6c10faf6f35a6aff48ac
kraken108 <jakob-danielsson@hotmail.com>
2016-05-15 03:01:32 +0200

    ...

--- integration/spelet/newmain.c
+++ integration/spelet/newmain.c
@@ -77,6 +77,7 @@ int main(int argc, char *argv[])
     }
 
     Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,2048);
+    Mix_VolumeMusic(20);
     Mix_Music *backgroundSound = Mix_LoadMUS("gta3.MP3");
 
 
